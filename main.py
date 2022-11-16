2a9b1478d3d9d785c9f9b6f14bd2fb692cc64b57
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-16 10:49:53 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def next_question():
+    print("\nLet's continue with you next question:")
+
+    print("""Someone who strongly asserts a position is often said to be "putting his" what down?
+A: Foot
+B: Head
+C: VCR remote
+D: Money """)
